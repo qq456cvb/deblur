7ce56a8c75d4939ecdf8d6c54fd3c830c6a93c9c
qq456cvb <447626601@qq.com>
2019-05-03 00:13:33 +0800

    replace sobel with custom gradient filter

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def shock_filter(img):
 
 # The algorithm is not clear stated in the paper, neither the parameters, try to implement, but failed
 if __name__ == '__main__':
-    ksize = 35
+    ksize = 25
     image = cv2.imread('lion.gif')
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY).astype(np.float32) / 255.
 
@@ -39,14 +39,16 @@ if __name__ == '__main__':
         # cv2.imshow('test', pyramids[-1])
         # cv2.waitKey()
 
-    tau_r = 0.2
-    tau_s = 0.2
+    tau_r = 0.1
+    tau_s = 0.05
     cv2.imshow('raw', image)
     cv2.waitKey(30)
     for i in reversed(range(n_pyramids)):
         img = pyramids[i]
-        Bx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
-        By = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+        # Bx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
+        Bx = signal.convolve2d(img, np.array([[-1, 0, 1]]) / 2, mode='same')
+        # By = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
+        By = signal.convolve2d(img, np.array([[-1], [0], [1]]) / 2, mode='same')
         convx = signal.convolve2d(Bx, np.ones((ksizes[i], ksizes[i])), mode='same')
         convy = signal.convolve2d(By, np.ones((ksizes[i], ksizes[i])), mode='same')
         r = np.linalg.norm(np.stack([convx, convy], -1), axis=-1) / \
@@ -59,16 +61,20 @@ if __name__ == '__main__':
         for it in range(10):
 
             # select edges
-            Ix = cv2.Sobel(I, cv2.CV_64F, 1, 0, ksize=3)
-            Iy = cv2.Sobel(I, cv2.CV_64F, 0, 1, ksize=3)
+            # Ix = cv2.Sobel(I, cv2.CV_64F, 1, 0, ksize=3)
+            Ix = signal.convolve2d(I, np.array([[-1, 0, 1]]) / 2, mode='same')
+            # Iy = cv2.Sobel(I, cv2.CV_64F, 0, 1, ksize=3)
+            Iy = signal.convolve2d(I, np.array([[-1], [0], [1]]) / 2, mode='same')
 
             shocked = shock_filter(I)
-            Ix_shocked = cv2.Sobel(shocked, cv2.CV_64F, 1, 0, ksize=3)
-            Iy_shocked = cv2.Sobel(shocked, cv2.CV_64F, 0, 1, ksize=3)
+            # Ix_shocked = cv2.Sobel(shocked, cv2.CV_64F, 1, 0, ksize=3)
+            Ix_shocked = signal.convolve2d(shocked, np.array([[-1, 0, 1]]) / 2, mode='same')
+            # Iy_shocked = cv2.Sobel(shocked, cv2.CV_64F, 0, 1, ksize=3)
+            Iy_shocked = signal.convolve2d(shocked, np.array([[-1], [0], [1]]) / 2, mode='same')
             # Is = shocked * np.heaviside(np.heaviside(r - tau_r, 0) * np.linalg.norm(np.stack([Ix_shocked, Iy_shocked], -1), axis=-1) - tau_s, 0)
             edges = np.stack([Ix_shocked, Iy_shocked], -1) * np.heaviside(np.heaviside(r - tau_r, 0) * np.linalg.norm(np.stack([Ix_shocked, Iy_shocked], -1), axis=-1) - tau_s, 0)[..., None]
             # edges = np.stack([cv2.Sobel(Is, cv2.CV_64F, 1, 0, ksize=3), cv2.Sobel(Is, cv2.CV_64F, 0, 1, ksize=3)], -1)
-            cv2.imshow('selected edges', edges[:, :, 0])
+            cv2.imshow('selected edges', edges[..., 0] / edges[..., 0].max())
             cv2.waitKey(30)
             fftgradb = np.fft.fft2(np.stack([Bx, By], -1), axes=[0, 1])
             fftedges = np.fft.fft2(edges, axes=[0, 1])
@@ -80,7 +86,7 @@ if __name__ == '__main__':
             k[ksizes[i] // 2 + 1:-ksizes[i] // 2, ksizes[i] // 2 + 1:-ksizes[i] // 2] = 0
             # how to handle sparse kernels???
             # k[k < 0] = 0
-            # k /= k.sum()
+            k /= k.sum()
             fftk = np.fft.fft2(k)
             # print(k.sum())
             cv2.imshow('kernel', k / k.max())
@@ -89,18 +95,24 @@ if __name__ == '__main__':
             scharr_x, scharr_y = np.zeros_like(k), np.zeros_like(k)
 
             # schar x
-            scharr_x[:2, :2] = np.array([[0, 1], [0, 1]])
-            scharr_x[-1, -1] = -1
-            scharr_x[-1, :2] = np.array([0, 1])
-            scharr_x[:2, -1] = np.array([-1, -1])
+            # scharr_x[:2, :2] = np.array([[0, 1], [0, 1]])
+            # scharr_x[-1, -1] = -1
+            # scharr_x[-1, :2] = np.array([0, 1])
+            # scharr_x[:2, -1] = np.array([-1, -1])
+            #
+            # scharr_y[:2, :2] = np.array([[0, 0], [1, 1]])
+            # scharr_y[-1, -1] = -1
+            # scharr_y[-1, :2] = np.array([-1, -1])
+            # scharr_y[:2, -1] = np.array([0, 1])
 
-            scharr_y[:2, :2] = np.array([[0, 0], [1, 1]])
-            scharr_y[-1, -1] = -1
-            scharr_y[-1, :2] = np.array([-1, -1])
-            scharr_y[:2, -1] = np.array([0, 1])
+            scharr_x[0, 1] = 1
+            scharr_x[0, -1] = -1
 
-            # scharr_x /= 255.
-            # scharr_y /= 255.
+            scharr_y[1, 0] = 1
+            scharr_y[-1, 0] = -1
+
+            scharr_x /= 2.
+            scharr_y /= 2.
 
             fftdx = np.fft.fft2(scharr_x)
             # cv2.imshow('fftdx', fftdx.astype(np.float32) / fftdx.astype(np.float32).max())
