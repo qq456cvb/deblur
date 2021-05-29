9d9293be131fa803eb438658427332bc4c006e36
qq456cvb <447626601@qq.com>
2021-05-29 11:04:03 +0800

    first phase finished

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ import numpy as np
 from scipy import signal
 
 
-LAMBDA = 2e-3
-
-
 def shock_filter(img):
     img = cv2.GaussianBlur(img, ksize=(3, 3), sigmaX=0)
-    it = 20
-    dt = 0.1
+    it = 10
+    dt = 0.01
     for i in range(it):
         Ix = signal.convolve2d(img, np.array([[-1, 0, 1]]) / 2, mode='same')
         Iy = signal.convolve2d(img, np.array([[-1], [0], [1]]) / 2, mode='same')
@@ -20,6 +17,13 @@ def shock_filter(img):
     # cv2.imshow('shock_filtered', img)
     # cv2.waitKey(0)
     return img
+
+
+# assume channel in the last
+def img_double(img):
+    res = np.zeros((img.shape[0] * 2, img.shape[1] * 2, *img.shape[2:]), img.dtype)
+    res[:img.shape[0], :img.shape[1]] = img
+    return res
 
 
 if __name__ == '__main__':
@@ -50,6 +54,7 @@ if __name__ == '__main__':
         
         # FIXME: border should not contribute to the final optimization
         ksize = ksizes[i]
+        half_ksize = ksize // 2
         Bx = signal.convolve2d(B, np.array([[-1, 0, 1]]) / 2, mode='same')
         By = signal.convolve2d(B, np.array([[-1], [0], [1]]) / 2, mode='same')
         convx = signal.convolve2d(Bx, np.ones((ksize, ksize)), mode='same')
@@ -57,8 +62,8 @@ if __name__ == '__main__':
         r = np.linalg.norm(np.stack([convx, convy], -1), axis=-1) / \
             (signal.convolve2d(np.linalg.norm(np.stack([Bx, By], -1), axis=-1), np.ones((ksize, ksize)),
                             mode='same') + 0.5 / 255.)
-        cv2.imshow('r map', r)
-        cv2.waitKey(30)
+        # cv2.imshow('r map', r)
+        # cv2.waitKey(30)
 
         grad_angle = np.arctan2(By, Bx)
         grad_angle[grad_angle < 0] += np.pi
@@ -68,12 +73,15 @@ if __name__ == '__main__':
             (grad_angle > np.pi / 2) |  (grad_angle < 3 * np.pi / 4),
             grad_angle > 3 * np.pi / 4
         ])
+        angle_masks[:, 0] = angle_masks[:, -1] = angle_masks[:, :, 0] = angle_masks[:, :, -1] = False
         # print(np.sum(angle_masks[0]), np.sum(angle_masks[1]), np.sum(angle_masks[2]), np.sum(angle_masks[3]))
         # import pdb; pdb.set_trace()
-        for j in range(10):
+        for j in range(30):
             I_tilde = shock_filter(L)
             I_tilde_grad = np.stack([signal.convolve2d(I_tilde, np.array([[-1, 0, 1]]) / 2, mode='same'),
                 signal.convolve2d(I_tilde, np.array([[-1], [0], [1]]) / 2, mode='same')], -1)
+            # I_tilde_grad[0] = I_tilde_grad[-1] = I_tilde_grad[:, 0] = I_tilde_grad[:, -1] = 0
+
             if j == 0:
                 tau_r_num = int(0.5 * np.sqrt(grad_angle.shape[0] * grad_angle.shape[1]) * ksize)
                 tau_s_num = 2 * ksize
@@ -81,90 +89,28 @@ if __name__ == '__main__':
                 tau_s = max([np.min([-np.partition(-I_tilde_grad[mask][..., k], tau_s_num)[tau_s_num] for mask in angle_masks]) for k in range(2)])
 
             I_grad_s = I_tilde_grad * np.heaviside(np.heaviside(r - tau_r, 0) * np.linalg.norm(I_tilde_grad, axis=-1) - tau_s, 0)[..., None]
-            cv2.imshow('selected edge', np.linalg.norm(I_grad_s, axis=-1))
-            cv2.waitKey()
-            tau_r /= 1.1
-            tau_s /= 1.1
-            L = B
             
-    exit()
-
-    n_pyramids = 1
-    pyramids = [image]
-    ksizes = [ksize]
-    for i in range(n_pyramids - 1):
-        pyramids.append(cv2.pyrDown(pyramids[-1]))
-        ksizes.append(ksize // 2)
-        if ksizes[-1] % 2 == 0:
-            ksizes[-1] += 1
-        # cv2.imshow('test', pyramids[-1])
-        # cv2.waitKey()
-
-    tau_r = 0.1
-    tau_s = 0.05
-    cv2.imshow('raw', image)
-    cv2.waitKey(30)
-    for i in reversed(range(n_pyramids)):
-        img = pyramids[i]
-        # Bx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
-        Bx = signal.convolve2d(img, np.array([[-1, 0, 1]]) / 2, mode='same')
-        # By = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
-        By = signal.convolve2d(img, np.array([[-1], [0], [1]]) / 2, mode='same')
-        convx = signal.convolve2d(Bx, np.ones((ksizes[i], ksizes[i])), mode='same')
-        convy = signal.convolve2d(By, np.ones((ksizes[i], ksizes[i])), mode='same')
-        r = np.linalg.norm(np.stack([convx, convy], -1), axis=-1) / \
-            (signal.convolve2d(np.linalg.norm(np.stack([Bx, By], -1), axis=-1), np.ones((ksizes[i], ksizes[i])),
-                               mode='same') + 0.5 / 255.)
-        cv2.imshow('r map', r)
-        cv2.waitKey(0)
-        I = img
-        B = img
-        for it in range(10):
-
-            # select edges
-            # Ix = cv2.Sobel(I, cv2.CV_64F, 1, 0, ksize=3)
-            Ix = signal.convolve2d(I, np.array([[-1, 0, 1]]) / 2, mode='same')
-            # Iy = cv2.Sobel(I, cv2.CV_64F, 0, 1, ksize=3)
-            Iy = signal.convolve2d(I, np.array([[-1], [0], [1]]) / 2, mode='same')
-
-            shocked = shock_filter(I)
-            # Ix_shocked = cv2.Sobel(shocked, cv2.CV_64F, 1, 0, ksize=3)
-            Ix_shocked = signal.convolve2d(shocked, np.array([[-1, 0, 1]]) / 2, mode='same')
-            # Iy_shocked = cv2.Sobel(shocked, cv2.CV_64F, 0, 1, ksize=3)
-            Iy_shocked = signal.convolve2d(shocked, np.array([[-1], [0], [1]]) / 2, mode='same')
-            # Is = shocked * np.heaviside(np.heaviside(r - tau_r, 0) * np.linalg.norm(np.stack([Ix_shocked, Iy_shocked], -1), axis=-1) - tau_s, 0)
-            edges = np.stack([Ix_shocked, Iy_shocked], -1) * np.heaviside(np.heaviside(r - tau_r, 0) * np.linalg.norm(np.stack([Ix_shocked, Iy_shocked], -1), axis=-1) - tau_s, 0)[..., None]
-            # edges = np.stack([cv2.Sobel(Is, cv2.CV_64F, 1, 0, ksize=3), cv2.Sobel(Is, cv2.CV_64F, 0, 1, ksize=3)], -1)
-            cv2.imshow('selected edges', edges[..., 0] / edges[..., 0].max())
-            cv2.waitKey(30)
-            fftgradb = np.fft.fft2(np.stack([Bx, By], -1), axes=[0, 1])
-            fftedges = np.fft.fft2(edges, axes=[0, 1])
-            # print(Ix.shape, fftedges.shape)
-            # TODO: actually this is circular convolution, should pad original image to represent normal convolution
-            fftk = (np.conjugate(fftedges[:, :, 0]) * fftgradb[:, :, 0] + np.conjugate(fftedges[:, :, 1]) * fftgradb[:, :, 1]) \
-                             / (np.square(np.absolute(fftedges[:, :, 0])) + np.square(np.absolute(fftedges[:, :, 1])))
+            # cv2.imshow('selected edge', np.linalg.norm(I_tilde_grad, axis=-1))
+            # if cv2.waitKey() == 27:
+            #     exit()
+            
+            # kernel estimation
+            fftgradb = np.fft.fft2(img_double(np.stack([Bx, By], -1)), axes=[0, 1])
+            fftedges = np.fft.fft2(img_double(I_grad_s), axes=[0, 1])
+            
+            fftk = (np.conjugate(fftedges[..., 0]) * fftgradb[..., 0] + np.conjugate(fftedges[..., 1]) * fftgradb[..., 1]) \
+                             / (np.square(np.absolute(fftedges[..., 0])) + np.square(np.absolute(fftedges[..., 1])) + 10.)
             k = np.real(np.fft.ifft2(fftk))
-            k[ksizes[i] // 2 + 1:-ksizes[i] // 2, ksizes[i] // 2 + 1:-ksizes[i] // 2] = 0
-            # how to handle sparse kernels???
-            # k[k < 0] = 0
-            k /= k.sum()
-            fftk = np.fft.fft2(k)
-            # print(k.sum())
-            cv2.imshow('kernel', k / k.max())
-            cv2.waitKey(30)
-            # Scharr operator, due to opencv doc, when kernel size is 3, solbel is scharr
-            scharr_x, scharr_y = np.zeros_like(k), np.zeros_like(k)
-
-            # schar x
-            # scharr_x[:2, :2] = np.array([[0, 1], [0, 1]])
-            # scharr_x[-1, -1] = -1
-            # scharr_x[-1, :2] = np.array([0, 1])
-            # scharr_x[:2, -1] = np.array([-1, -1])
-            #
-            # scharr_y[:2, :2] = np.array([[0, 0], [1, 1]])
-            # scharr_y[-1, -1] = -1
-            # scharr_y[-1, :2] = np.array([-1, -1])
-            # scharr_y[:2, -1] = np.array([0, 1])
+            kernel = np.zeros_like(k)
+            kernel[:half_ksize+1, :half_ksize+1] = k[:half_ksize+1, :half_ksize+1]
+            kernel[-half_ksize:, -half_ksize:] = k[-half_ksize:, -half_ksize:]
+            kernel[kernel < 0] = 0
+            kernel /= kernel.sum()
+            # print(kernel[:half_ksize+1, :half_ksize+1])
+            # print(kernel[-half_ksize:, -half_ksize:])
+            # cv2.imshow('kernel estimation', kernel)
+            # cv2.waitKey()
+            scharr_x, scharr_y = np.zeros_like(kernel), np.zeros_like(kernel)
 
             scharr_x[0, 1] = 1
             scharr_x[0, -1] = -1
@@ -176,16 +122,22 @@ if __name__ == '__main__':
             scharr_y /= 2.
 
             fftdx = np.fft.fft2(scharr_x)
-            # cv2.imshow('fftdx', fftdx.astype(np.float32) / fftdx.astype(np.float32).max())
-            # cv2.waitKey()
             fftdy = np.fft.fft2(scharr_y)
-
-            # artifact induced, don't know why
-            latent = np.real(np.fft.ifft2((np.conjugate(fftk) * np.fft.fft2(B) + LAMBDA * (np.conjugate(fftdx) * np.fft.fft2(edges[:, :, 0]) + np.conjugate(fftdy) * np.fft.fft2(edges[:, :, 1])))
-                                  / (np.conjugate(fftk) * fftk + LAMBDA * (np.conjugate(fftdx) * fftdx + np.conjugate(fftdy) * fftdy))))
-            latent = np.clip(latent, 0, 1)
-            cv2.imshow('latent%d' % it, latent)
-            cv2.waitKey()
-            tau_s /= 1.1
+            
+            fftk = np.fft.fft2(kernel)
+            latent = np.real(np.fft.ifft2((np.conjugate(fftk) * np.fft.fft2(img_double(B)) + 2e-3 * (np.conjugate(fftdx) * fftedges[..., 0] + np.conjugate(fftdy) * fftedges[..., 1]))
+                                  / (np.conjugate(fftk) * fftk + 2e-3 * (np.conjugate(fftdx) * fftdx + np.conjugate(fftdy) * fftdy))))
+            L = np.clip(latent[:latent.shape[0] // 2, :latent.shape[1] // 2], 0, 1)
+            cv2.imshow('latent image estimation', L)
+            if cv2.waitKey(30) == 27:
+                exit()
             tau_r /= 1.1
-            I = latent
+            tau_s /= 1.1
+            
+        if i < len(pyramids) - 1:
+            L = cv2.resize(L, (pyramids[i+1].shape[1], pyramids[i+1].shape[0]))
+        vis_kernel = np.zeros((ksize, ksize))
+        vis_kernel[:half_ksize, :half_ksize] = kernel[-half_ksize:, -half_ksize:]
+        vis_kernel[half_ksize:, half_ksize:] = kernel[:half_ksize+1, :half_ksize+1]
+        cv2.imshow('kernel', vis_kernel / vis_kernel.max())
+        cv2.waitKey(0)
